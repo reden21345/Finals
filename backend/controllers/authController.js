010d5e8379dcc994d54124e021e03d8ce5a75725
reden21345 <mirandilla395@gmail.com>
2023-11-19 13:29:05 +0800

    JWT token in cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken');
 
 
 //Register a user => /api/v1/register
@@ -19,18 +20,7 @@ exports.registerUser = catchAsyncErrors( async (req, res, next) => {
         }
     })
 
-     const token = user.getJwtToken();
-
-      res.status(201).json({
-      	success:true,
-      	user,
-     	token
-      })
-
-    // res.status(201).json({
-    //     success: true,
-    //     user
-    // })
+    sendToken(user, 200, res);
 })
 
 
@@ -57,10 +47,5 @@ exports.loginUser = catchAsyncErrors( async (req, res, next) => {
             return next(new ErrorHandler('Invalid email or password', 401));
         }
 
-        const token = user.getJwtToken();
-
-        res.status(200).json({
-            success: true,
-            token
-        })
+        sendToken(user, 200, res);
 })
