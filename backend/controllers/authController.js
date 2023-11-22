1dd2ad02fdc20029fddf9c4e55894a47444d4a71
reden21345 <mirandilla395@gmail.com>
2023-11-22 21:17:49 +0800

    Reset Password

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,9 +1,9 @@
 const User = require('../models/user');
-
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
 
 
 //Register a user => /api/v1/register
@@ -91,6 +91,37 @@ exports.forgotPassword = catchAsyncErrors( async (req, res, next) => {
 
         return next(new ErrorHandler(error.message, 500));
     }
+})
+
+
+// Reset Password => /api/v1/password/reset/:token
+exports.resetPassword = catchAsyncErrors( async (req, res, next) => {
+
+    // Hash URL token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now()}
+    })
+
+    if (!user) { 
+        return next(new ErrorHandler('Password reset token is invalid or has been expired', 400));
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler('Password does not match'), 400);
+    }
+
+    // Setup new Password
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    sendToken(user ,200 ,res);
+
 })
 
 
