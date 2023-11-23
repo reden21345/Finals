adfb27155980032cd2792185164220ebbcbfe60f
reden21345 <mirandilla395@gmail.com>
2023-11-23 15:16:41 +0800

    Admin Routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,10 +8,12 @@ const { registerUser,
     resetPassword, 
     getUserProfile, 
     updatePassword, 
-    updateProfile
+    updateProfile,
+    allUsers,
+    getUserDetails
 } = require('../controllers/authController');
 
-const { isAuthenticatedUser } = require('../middlewares/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 
 router.route('/register').post(registerUser);
@@ -25,6 +27,10 @@ router.route('/logout').get(logout);
 router.route('/me').get( isAuthenticatedUser, getUserProfile);
 router.route('/password/update').put(isAuthenticatedUser, updatePassword);
 router.route('/me/update').put(isAuthenticatedUser, updateProfile);
+
+// Admin Routes
+router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
+router.route('/admin/user/:id').get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails);
 
 
 module.exports = router;
