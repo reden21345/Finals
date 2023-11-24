09bd4e837b559ca0bddc6c61b4518e1203cf238f
reden21345 <mirandilla395@gmail.com>
2023-11-24 12:31:07 +0800

    Get single order & Get logged in user orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -34,3 +34,28 @@ exports.newOrder = catchAyncErrors( async (req, res, next) => {
         order
     })
 })
+
+// Get single order   =>   /api/v1/order/:id
+exports.getSingleOrder = catchAyncErrors( async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email');
+    
+    if (!order) {
+        return next(new ErrorHandler('No Order found with this ID', 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        order
+    })
+})
+
+// Get logged in user orders   =>   /api/v1/orders/me
+exports.myOrders = catchAyncErrors( async (req, res, next) => {
+    const orders = await Order.find({ user: req.user.id });
+
+    res.status(200).json({
+        success: true,
+        count: orders.length,
+        orders
+    })
+})
