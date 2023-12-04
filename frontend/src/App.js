7587c9425149e209a227764dfd4e2bb454c7e9ea
reden21345 <mirandilla395@gmail.com>
2023-12-04 09:22:35 +0800

    Sidebar and Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,14 +8,17 @@ import Footer from './components/layout/Footer';
 import Home from './components/Home';
 import CoffeeDetails from './components/coffee/CoffeeDetails';
 
+// Cart
 import Cart from './components/cart/Cart';
 import Shipping from './components/cart/Shipping';
 import ConfirmOrder from './components/cart/ConfirmOrder';
 import OrderSuccess from './components/cart/OrderSuccess'
 
+// Order
 import ListOrders from './components/order/ListOrders'
 import OrderDetails from './components/order/OrderDetails';
 
+// User
 import Login from './components/user/login';
 import Register from './components/user/Register';
 import Profile from './components/user/Profile';
@@ -28,6 +31,9 @@ import ProtectedRoute from './components/route/ProtectedRoute';
 import { loadUser } from './actions/userActions';
 import store from './store';
 import axios from 'axios';
+
+// Admin
+import Dashboard from './components/admin/Dashboard';
 
 // Payments
 import Payment from './components/cart/Payment';
@@ -86,8 +92,14 @@ function App() {
                   <Route path="/payment" element={<Payment/>} />
                 </Routes>
               </Elements>
-            }
+          }
+
         </div>
+
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} exact/>
+          </Routes>
+          
         <Footer />
       </div>
     </Router>
---
+++ frontend/src/components/admin/Sidebar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar-wrapper">
+            <nav id="sidebar">
+                <ul className="list-unstyled components">
+                    <li>
+                        <Link to="/dashboard"><i className="fa fa-tachometer"></i> Dashboard</Link>
+                    </li>
+
+                    <li>
+                        <a href="#coffeeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle"><i
+                            className="fa fa-coffee-hunt"></i> Coffees</a>
+                        <ul className="collapse list-unstyled" id="coffeeSubmenu">
+                            <li>
+                                <Link to="/admin/coffees"><i className="fa fa-clipboard"></i> All</Link>
+                            </li>
+
+                            <li>
+                                <Link to="/admin/coffee"><i className="fa fa-plus"></i> Create</Link>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <Link to="/admin/orders"><i className="fa fa-shopping-basket"></i> Orders</Link>
+                    </li>
+
+                    <li>
+                        <Link to="/admin/users"><i className="fa fa-users"></i> Users</Link>
+                    </li>
+
+                    <li>
+                        <Link to="/admin/reviews"><i className="fa fa-star"></i> Reviews</Link>
+                    </li>
+
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Sidebar
