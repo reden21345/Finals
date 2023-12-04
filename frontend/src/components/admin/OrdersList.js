6df977cc02e4230452ee451c58860d7052d7f07f
reden21345 <mirandilla395@gmail.com>
2023-12-04 15:13:35 +0800

    Delete Order

--- frontend/src/components/admin/OrdersList.js
+++ frontend/src/components/admin/OrdersList.js
@@ -8,17 +8,17 @@ import Sidebar from './Sidebar'
 
 import { useAlert } from 'react-alert'
 import { useDispatch, useSelector } from 'react-redux'
-import { allOrders, clearErrors } from '../../actions/orderActions'
-// import { DELETE_ORDER_RESET } from '../../constants/orderConstants'
+import { allOrders, deleteOrder, clearErrors } from '../../actions/orderActions'
+import { DELETE_ORDER_RESET } from '../../constants/orderConstants'
 
-const OrdersList = ({ history }) => {
+const OrdersList = () => {
 
     const navigate = useNavigate();
     const alert = useAlert();
     const dispatch = useDispatch();
 
     const { loading, error, orders } = useSelector(state => state.allOrders);
-    // const { isDeleted } = useSelector(state => state.order)
+    const { isDeleted } = useSelector(state => state.order)
 
     useEffect(() => {
         dispatch(allOrders());
@@ -28,17 +28,17 @@ const OrdersList = ({ history }) => {
             dispatch(clearErrors())
         }
 
-        // if (isDeleted) {
-        //     alert.success('Order deleted successfully');
-        //     history.push('/admin/orders');
-        //     dispatch({ type: DELETE_ORDER_RESET })
-        // }
+        if (isDeleted) {
+            alert.success('Order deleted successfully');
+            navigate('/admin/orders');
+            dispatch({ type: DELETE_ORDER_RESET })
+        }
 
-    }, [dispatch, alert, error])
+    }, [dispatch, alert, error, isDeleted, navigate])
 
-    // const deleteOrderHandler = (id) => {
-    //     dispatch(deleteOrder(id))
-    // }
+    const deleteOrderHandler = (id) => {
+        dispatch(deleteOrder(id))
+    }
 
     const setOrders = () => {
         const data = {
@@ -83,7 +83,7 @@ const OrdersList = ({ history }) => {
                     <Link to={`/admin/order/${order._id}`} className="btn btn-primary py-1 px-2">
                         <i className="fa fa-eye"></i>
                     </Link>
-                    <button className="btn btn-danger py-1 px-2 ml-2" >
+                    <button className="btn btn-danger py-1 px-2 ml-2" onClick={() => deleteOrderHandler(order._id)}>
                         <i className="fa fa-trash"></i>
                     </button>
                 </Fragment>
